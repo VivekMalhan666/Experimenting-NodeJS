1b3a88c1836338aaa2a8be2b2ee23463d3f1a9fd
VivekMalhan666 <amalhan43@gmail.com>
2020-08-16 22:18:54 +0530

    setting up node

---
+++ node-examples/index.js
@@ -0,0 +1,20 @@
+var rect = require('./rectangle');
+const rectangle = require('./rectangle');
+
+function solveRect(l,b){
+    console.log("Solving for rectangle with l = "+ l + " and b = " +b);
+
+    rect (l,b,(err,rectangle)=>{
+        if(err){
+            console.log("ERROR" + err.message);
+        }
+        else{
+            console.log("The area of the rectangle of dimensions l = "
+            + l + " and b = " + b + " is " + rectangle.area());
+        console.log("The perimeter of the rectangle of dimensions l = "
+            + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+}
+
+solveRect(3,5);
